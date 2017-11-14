8e2afea7bc05a9a533a4d56863c7001f75441e80
Antonio Jr <acdcjunior@gmail.com>
2017-11-14 00:06:06 -0200

    forgot: add @SpringBootApplication/main() soh `gradlew bootRun` works

--- sb-aaa/src/main/java/sb/bug/aaa/ClassAAA.java
+++ sb-aaa/src/main/java/sb/bug/aaa/ClassAAA.java
@@ -1,11 +1,16 @@
 package sb.bug.aaa;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import sb.bug.bbb.ClassBBB;
 
+@SpringBootApplication
 public class ClassAAA {
 
-    public ClassAAA(ClassBBB c) {
-        System.out.println(c);
+    public static void main(String[] args) {
+        System.out.println(ClassBBB.class.getCanonicalName());
+        SpringApplication.run(ClassAAA.class, args);
     }
 
 }
